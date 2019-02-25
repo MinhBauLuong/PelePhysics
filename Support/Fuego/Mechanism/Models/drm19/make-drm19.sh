bac1498ec99979089a4a8ae6218ca87482f06f02
Anne Felden <Enna@lbl.gov>
2019-02-24 20:01:22 -0800

    [FUEGO] update make.sh for drm19 and dodecane_wang

--- Support/Fuego/Mechanism/Models/drm19/make-drm19.sh
+++ Support/Fuego/Mechanism/Models/drm19/make-drm19.sh
@@ -14,7 +14,5 @@ ${FUEGO_PYTHON} ${FMC} -mechanism=${CHEMINP} -thermo=${THERMINP} -name=${CHEMC}
 echo Compiling ${FINALFILE}...
 cat ${CHEMC} \
           ${HEADERDIR}/header.start\
-          ${HEADERDIR}/header.mec   ${CHEMINP}\
-          ${HEADERDIR}/header.therm ${THERMINP}\
           ${HEADERDIR}/header.end > ${FINALFILE}
 rm -f ${CHEMC} ${CHEMLK} ${LOG} 
