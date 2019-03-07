771698941109760fe37e70ced04473a4ef1be0e8
Anne Felden <Enna@lbl.gov>
2019-03-07 13:01:27 -0800

    [Tutorial] Fix stuff

--- Testing/Exec/ReactEvalCVODE_fromC_Precond/main.cpp
+++ Testing/Exec/ReactEvalCVODE_fromC_Precond/main.cpp
@@ -144,8 +144,8 @@ int main (int argc,
     BoxArray ba;
     Geometry geom;
     IntVect dom_lo(IntVect(D_DECL(0,0,0)));
-    //IntVect dom_hi(IntVect(D_DECL(n_cell-1, 0, 0)));
-    IntVect dom_hi(IntVect(D_DECL(n_cell-1, max_grid_size-1, 0)));
+    IntVect dom_hi(IntVect(D_DECL(n_cell-1, 0, 0)));
+    //IntVect dom_hi(IntVect(D_DECL(n_cell-1, max_grid_size-1, 0)));
     Box domain(dom_lo, dom_hi);
 
     // Initialize the boxarray "ba" from the single box "bx"
@@ -156,7 +156,7 @@ int main (int argc,
     const IntVect ChunkSize = IntVect::TheUnitVector(); //parent->maxGridSize(level);
     IntVect chunk(ChunkSize);
     chunk[0] = 1; //max_grid_size;
-    chunk[1] = max_grid_size; 
+    chunk[1] = 1;//max_grid_size; 
     //chunk[2] = 1; 
     ba.maxSize(chunk);
 
